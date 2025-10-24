1042ef4f638e167246679e33f80acbaea8fd1359
Pedro0152 <kresh0152@gmail.com>
2025-10-24 13:29:48 -0300

    removed duplicated model 'account.group' in account.py

--- l10n_cu/models/account.py
+++ l10n_cu/models/account.py
@@ -96,37 +96,6 @@ class AccountChartTemplate(models.AbstractModel):
                 {'acc_name': 'Otros valores en caja', 'account_type': 'cash', 'code':'FOTROS'}]
         return journals_to_create[1:]
 
-class AccountGroupTemplate(models.Model):
-    _inherit = "account.group"
-
-    reconcile = fields.Boolean(string='Allow Reconciliation')   
-    expense_element_detailed = fields.Boolean()
-    account_type = fields.Selection(
-        selection=[
-            ("asset_receivable", "Receivable"),
-            ("asset_cash", "Bank and Cash"),
-            ("asset_current", "Current Assets"),
-            ("asset_non_current", "Non-current Assets"),
-            ("asset_prepayments", "Prepayments"),
-            ("asset_fixed", "Fixed Assets"),
-            ("liability_payable", "Payable"),
-            ("liability_credit_card", "Credit Card"),
-            ("liability_current", "Current Liabilities"),
-            ("liability_non_current", "Non-current Liabilities"),
-            ("equity", "Equity"),
-            ("equity_unaffected", "Current Year Earnings"),
-            ("income", "Income"),
-            ("income_other", "Other Income"),
-            ("expense", "Expenses"),
-            ("expense_depreciation", "Depreciation"),
-            ("expense_direct_cost", "Cost of Revenue"),
-            ("off_balance", "Off-Balance Sheet"),
-        ],
-        string="Type", required=False, help="Account Type is used for information purpose, to generate country-specific legal reports, and set the rules to close a fiscal year and generate opening entries."
-    )
-    note = fields.Text("Content and Usage")
-
-
 class AccountGroup(models.Model):
     _inherit = "account.group"
 
