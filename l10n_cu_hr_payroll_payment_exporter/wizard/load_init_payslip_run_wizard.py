f56cebfd943851eb72ccc84f0ad2a5b1050c4849
Ing. José A. Hernández Bustio <jbustio@gmail.com>
2025-11-10 06:10:15 -0500

    [UPD] Necesidad de atualizar vacaciones

--- l10n_cu_hr_payroll_payment_exporter/wizard/load_init_payslip_run_wizard.py
+++ l10n_cu_hr_payroll_payment_exporter/wizard/load_init_payslip_run_wizard.py
@@ -26,56 +26,51 @@ class LoadInitPayslipRun(models.TransientModel):
                 'El formato del fichero tiene que ser xlsx y tiene que tener los campos predeterminados')
 
         sheet = book.sheets()[0]
-        # vac = self.env['hr.vacaciones'].search([])
-        nom = self.env['hr.payslip.run'].search([('state', 'in', ['done', 'close'])])
+        
+        for i in range(1, sheet.nrows):
+            dias = sheet.cell(i, 2).value
+            importe = sheet.cell(i, 3).value
+            ci = sheet.cell(i, 4).value
+            if isinstance(ci, float) and ci.is_integer():
+                ci = str(int(ci))
+            else:
+                ci = str(ci)
+            _logger.info("No. Identificacion %s " % ci)
+            empleados = self.env['hr.employee'].search([('identification_id', '=', ci)])
+            _logger.info("Empleado %s " % empleados.name)
+            if empleados:   
+                if empleados.vaca_dias_acum_init == 0:
+                    empleados.vaca_dias_acum_init = dias
+                    empleados.vaca_imp_acum_init = importe
+                    empleados.vaca_dias_acum = dias
+                    empleados.vaca_imp_acum = importe
 
-        if len(nom) > 0:
-            raise ValidationError('Ya se inicializaron los datos, hay nominas en estado hecho o cerradas')
-        else:
-            for i in range(1, sheet.nrows):
-                dias = sheet.cell(i, 2).value
-                importe = sheet.cell(i, 3).value
-                ci = sheet.cell(i, 4).value
-                if isinstance(ci, float) and ci.is_integer():
-                    ci = str(int(ci))
+                    vals = {
+                        'hr_payslip_run_id': 0,
+                        'company_id': 1,
+                        'dias_inicial': round(empleados.vaca_dias_acum_init, 2),
+                        'importe_inicial': round(empleados.vaca_imp_acum_init, 2),
+                        'dias_ganado': 0,
+                        'importe_ganado': 0,
+                        'dias_pagado': 0,
+                        'importe_pagado': 0,
+                        'dias_final': round(empleados.vaca_dias_acum_init, 2),
+                        'importe_final': round(empleados.vaca_imp_acum_init, 2),
+                        'date_start': datetime.now(),
+                        'date_end': datetime.now(),
+                        'create_date': datetime.now(),
+                    }
+
+                    self.env['hr.vacaciones'].create(vals)
                 else:
-                    ci = str(ci)
-                _logger.info("No. Identificacion %s " % ci)
-                empleados = self.env['hr.employee'].search([('identification_id', '=', ci)])
-                _logger.info("Empleado %s " % empleados.name)
-                if empleados:   
-                    if empleados.vaca_dias_acum_init == 0:
-                        empleados.vaca_dias_acum_init = dias
-                        empleados.vaca_imp_acum_init = importe
-                        empleados.vaca_dias_acum = dias
-                        empleados.vaca_imp_acum = importe
-
-                        vals = {
-                            'hr_payslip_run_id': 0,
-                            'company_id': 1,
-                            'dias_inicial': round(empleados.vaca_dias_acum_init, 2),
-                            'importe_inicial': round(empleados.vaca_imp_acum_init, 2),
-                            'dias_ganado': 0,
-                            'importe_ganado': 0,
-                            'dias_pagado': 0,
-                            'importe_pagado': 0,
-                            'dias_final': round(empleados.vaca_dias_acum_init, 2),
-                            'importe_final': round(empleados.vaca_imp_acum_init, 2),
-                            'date_start': datetime.now(),
-                            'date_end': datetime.now(),
-                            'create_date': datetime.now(),
-                        }
-
-                        self.env['hr.vacaciones'].create(vals)
-                    else:
-                        empleados.vaca_dias_acum_init = dias
-                        empleados.vaca_imp_acum_init = importe
-                        empleados.vaca_dias_acum = dias
-                        empleados.vaca_imp_acum = importe
-                        vacas = self.env['hr.vacaciones'].search([('employee_id', '=', empleados.id)])
-                        vacas.write({
-                            'dias_inicial': round(empleados.vaca_dias_acum_init, 2),
-                            'importe_inicial': round(empleados.vaca_imp_acum_init, 2),
-                            'dias_final': round(empleados.vaca_dias_acum_init, 2),
-                            'importe_final': round(empleados.vaca_imp_acum_init, 2),
-                        })
+                    empleados.vaca_dias_acum_init = dias
+                    empleados.vaca_imp_acum_init = importe
+                    empleados.vaca_dias_acum = dias
+                    empleados.vaca_imp_acum = importe
+                    vacas = self.env['hr.vacaciones'].search([('employee_id', '=', empleados.id)])
+                    vacas.write({
+                        'dias_inicial': round(empleados.vaca_dias_acum_init, 2),
+                        'importe_inicial': round(empleados.vaca_imp_acum_init, 2),
+                        'dias_final': round(empleados.vaca_dias_acum_init, 2),
+                        'importe_final': round(empleados.vaca_imp_acum_init, 2),
+                    })
