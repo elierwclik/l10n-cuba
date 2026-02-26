3d1aeacffee4fc4bdd8773b625ff897d7f9f96d4
Jose A. Hernandez Bustio <jbustio@gmail.com>
2026-02-26 15:56:09 -0500

    [FIX] Otras entradas

--- l10n_cu_hr_payroll_enterprise/models/hr_payslip.py
+++ l10n_cu_hr_payroll_enterprise/models/hr_payslip.py
@@ -103,6 +103,14 @@ class HrPayslipRun(models.Model):
             # if sheet.state == 'draft':
             #     sheet.state = 'calculate'
 
+class HrRuleInput(models.Model):
+    _name = 'hr.rule.input'
+    _description = 'Salary Rule Input'
+
+    name = fields.Char(string='Description', required=True)
+    code = fields.Char(required=True, help="The code that can be used in the salary rules")
+    #input_id = fields.Many2one('hr.salary.rule', string='Salary Rule Input', required=True)
+
 class HrPayrollOtherInput(models.Model):
     _name = 'hr.payroll.other.input'
     _description = "Payroll Other Input"
